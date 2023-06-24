e275081fb0e9d674925be54c5cd8263199194c37
ISAAC RITHARSON P <isaacritharson@karunya.edu.in>
2023-06-25 01:14:23 +0530

    Create Create a table employee_manager.sql

---
+++ Creating and Managing Tables/Create a table employee_manager.sql
@@ -0,0 +1,10 @@
+CREATE TABLE employee_manager_1041
+(Emp_ID Number(6) NOT NULL,
+Emp_last_name Varchar2(25),
+Emp_mid_name Varchar2(25),
+Emp_first_name Varchar2(25),
+Emp_email Varchar2(20),
+Department Varchar2(10),
+Grade Number(3),
+PRIMARY KEY (Emp_ID)
+);
