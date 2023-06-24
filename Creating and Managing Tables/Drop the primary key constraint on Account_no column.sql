52d4e0c65322ea43fb617a8cf852074df1658db5
ISAAC RITHARSON P <isaacritharson@karunya.edu.in>
2023-06-25 01:21:11 +0530

    Create Drop the primary key constraint on Account_no column.sql

---
+++ Creating and Managing Tables/Drop the primary key constraint on Account_no column.sql
@@ -0,0 +1,8 @@
+ALTER TABLE customer_details_1041
+DROP PRIMARY KEY;
+
+/*Truncate the table customer_details.*/
+TRUNCATE TABLE customer_details_1041;
+
+/*Drop the table customer_details.*/
+DROP TABLE customer_details_1041
