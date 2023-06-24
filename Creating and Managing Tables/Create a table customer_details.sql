74c88117dff9e00f4dc76c167d651b7d3a8ca09c
ISAAC RITHARSON P <isaacritharson@karunya.edu.in>
2023-06-25 01:13:39 +0530

    Create Create a table customer_details.sql

---
+++ Creating and Managing Tables/Create a table customer_details.sql
@@ -0,0 +1,10 @@
+CREATE TABLE customer_details_1041
+(Cust_ID number(5) NOT NULL,
+Cust_last_name Varchar2(20) NOT NULL,
+Cust_mid_name Varchar2(4),
+Cust_first_name Varchar2(20),
+Account_no Number(5) NOT NULL PRIMARY KEY,
+Account_type Varchar2(10) NOT NULL,
+Bank_branch Varchar2(25) NOT NULL,
+Cust_email Varchar2(30)
+);
